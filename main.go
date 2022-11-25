d57d47b650ae5af1d968c977580ae4acd7f0c1df
Mehmet Okdemir <mehmetokdemir92@gmail.com>
2022-11-26 02:13:39 +0300

    Added new repositories, services and handlers

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/mehmetokdemir/currency-conversion-service/internal/handlers"
 	"github.com/mehmetokdemir/currency-conversion-service/internal/repositories"
 	"github.com/mehmetokdemir/currency-conversion-service/internal/services"
+	"github.com/mehmetokdemir/currency-conversion-service/middleware"
 	swaggerFiles "github.com/swaggo/files"
 	swagger "github.com/swaggo/gin-swagger"
 	"log"
@@ -29,26 +30,56 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// CURRENCY SERVICE
 	currencyService := services.NewCurrencyService(cache.New(5*time.Minute, 10*time.Minute))
 	currencyService.SetLocalCacheToCurrencies()
 
+	// ACCOUNT SERVICE
+	accountRepository := repositories.NewAccountRepository(config.Connect(serviceConfig.Db))
+	if err = accountRepository.Migration(); err != nil {
+		log.Fatal(err)
+	}
+	accountService := services.NewAccountService(accountRepository, serviceConfig)
+	accountHandler := handlers.NewAccountHandler(accountService, currencyService)
+
+	// USER SERVICE
 	userRepository := repositories.NewUserRepository(config.Connect(serviceConfig.Db))
 	if err = userRepository.Migration(); err != nil {
 		log.Fatal(err)
 	}
+	userService := services.NewUserService(userRepository, serviceConfig, currencyService)
+	userHandler := handlers.NewUserHandler(userService, accountService)
 
-	userService := services.NewUserService(userRepository, serviceConfig)
-	userHandler := handlers.NewUserHandler(userService, currencyService)
-
-	// ACCOUNT SERVICE
+	// EXCHANGE SERVICE
+	exchangeRepository := repositories.NewExchangeRepository(config.Connect(serviceConfig.Db))
+	if err = exchangeRepository.Migration(); err != nil {
+		log.Fatal(err)
+	}
+	exchangeService := services.NewExchangeService(exchangeRepository, currencyService, accountService)
+	exchangeHandler := handlers.NewExchangeHandler(currencyService, exchangeService)
 
 	router := gin.New()
 	router.Use(gin.Recovery())
 
+	// User Routes
 	userGroup := router.Group("/user")
 	userGroup.Use()
 	{
 		userHandler.UserRoutes(userGroup)
+	}
+
+	// Account Routes
+	accountGroup := router.Group("/account")
+	accountGroup.Use(middleware.AuthMiddleware())
+	{
+		accountHandler.AccountRoutes(accountGroup)
+	}
+
+	// Exchange routes
+	exchangeGroup := router.Group("/exchange")
+	exchangeGroup.Use(middleware.AuthMiddleware())
+	{
+		exchangeHandler.ExchangeRoutes(exchangeGroup)
 	}
 
 	router.GET("/swagger/*any", swagger.WrapHandler(swaggerFiles.Handler))
